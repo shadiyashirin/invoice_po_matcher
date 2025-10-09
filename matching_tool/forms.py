5edb0aa43692e92433a5b4bbaefc4281cdfa0d00
shadiyashirin <vshadiyashirin2003@gmail.com>
2025-10-09 19:35:41 +0530

    render.yaml-changes

--- matching_tool/forms.py
+++ matching_tool/forms.py
@@ -1,14 +1,6 @@
 from django import forms
 from .models import Document
 
-'''class DocumentUploadForm(forms.ModelForm):
-    class Meta:
-        model = Document
-        fields = ['document_type', 'file']
-        widgets = {
-            'document_type': forms.Select(attrs={'class': 'form-control'}),
-            'file': forms.FileInput(attrs={'class': 'form-control'}),
-        }'''
 
 class DocumentForm(forms.ModelForm):
     class Meta:
--- matching_tool/models.py
+++ matching_tool/models.py
@@ -11,9 +11,6 @@ class UploadedDocument(models.Model):
     file = models.FileField(upload_to='documents/')
     document_type = models.CharField(max_length=10, choices=DOCUMENT_TYPES)
     uploaded_at = models.DateTimeField(auto_now_add=True)
-    # We can add fields for extracted data later if we want to store it
-    # vendor = models.CharField(max_length=255, blank=True, null=True)
-    # total_amount = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
 
     def __str__(self):
         return self.file.name
